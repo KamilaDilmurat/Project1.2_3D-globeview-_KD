653fa9595e48e486a268608811ee653a4fdb53fe
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-22 20:44:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-require(["esri/Map", "esri/views/SceneView"], (Map, SceneView) => {
+require([
+        "esri/Map",
+        "esri/views/SceneView",
+        "esri/layers/Layer"
+      ], function (Map, SceneView, Layer) {
         const map = new Map({
           basemap: "topo-vector",
-          ground: "world-elevation"      
+          ground: "world-elevation" 
         });
 
         const view = new SceneView({
-          container: "viewDiv",
           map: map,
-          scale: 50000000,
-          center: [-104.99, 39.74] // longitude, latitude
+          container: "viewDiv",
+         scale: 500000,
+          center: [87.5, 43.8]
         });
-      });
+
+        const layer = Layer.fromPortalItem({
+          portalItem: {
+            id: "2cdad2d703344d1fa2681bb5fb9fe221"
+          }
+        })
+          .then(addLayer)
+          .catch(rejection);
+  
+  const layer = Layer.fromPortalItem({
+          portalItem: {
+            id: "ec224b6e776c44ff8169d1f0fa27e5f2"
+          }
+        })
+          .then(addLayer)
+          .catch(rejection);
+
+        // Adds the layer to the map once it loads
+        function addLayer(layer) {
+          map.add(layer);
+        }
+        function rejection(error) {
+          console.log("Layer failed to load: ", error);
+        }
+      })
